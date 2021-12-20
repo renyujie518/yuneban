e5e2ffc6ec0a29f2681279b21650f329d1ef6681
renyujie518 <903356741@qq.com>
2021-12-20 17:11:24 +0800

    server后端项目搭建

---
+++ yeb-server/src/main/java/com/renyujie/server/YebApplication.java
@@ -0,0 +1,20 @@
+package com.renyujie.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author renyujie518
+ * @version 1.0.0
+ * @ClassName YebApplication.java
+ * @Description 启动类
+ * @createTime 2021年12月20日 17:03:00
+ */
+@SpringBootApplication
+@MapperScan("com.renyujie.server.mapper")
+public class YebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YebApplication.class, args);
+    }
+}
